7e7af532c89c157356d393aa93273fc09be07f4d
Tugrul Ates <me@tugrulates.com>
2023-01-18 20:37:03 +0000

    fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -100,12 +100,14 @@ def test_tags_note_missing() -> None:
     assert "missing-dir" in result.stdout
 
 
-def test_save_vault_to_config() -> None:
+def test_save_vault_to_config(vault: Path, tmp_path: Path) -> None:
     """Test skipping vault on subsequent invocations."""
-    result = runner.invoke(cli.app, "--vault . list")
+    new_vault = vault.with_suffix(".new")
+    vault.rename(new_vault)
+    result = runner.invoke(cli.app, f"--vault {new_vault} list")
     assert result.exit_code == 0
     cfg = config.load()
-    assert cfg.vault == Path(".")
+    assert cfg.vault == new_vault
 
 
 def test_note_list() -> None:
